de82c171ecdbbd930d3f95e30a70821e65b3af3c
Hubert Łępicki <hubert.lepicki@amberbit.com>
2022-12-02 12:19:40 +0100

    Working version with hooks / providers extracted

--- apps/shopper_web/assets/js/main.jsx
+++ apps/shopper_web/assets/js/main.jsx
@@ -1,20 +1,19 @@
-import * as React from 'react';
-import {useState, useRef, useEffect, useLayoutEffect} from 'react';
+import React, {useState, useRef, useEffect, useLayoutEffect} from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { useChannel } from './use_channel';
-import { PhoenixSocketProvider } from './phoenix_socket_context';
-import * as jsondiffpatch from "rfc6902";
+import { StateChannelProvider, PhoenixSocketProvider, useStateChannel } from './vendor/state_channel_react';
 
-const Form = ({state, pushMessage, clientVersion, serverVersion}) => {
+const Form = () => {
+  const { state, channel, pushMessage } = useStateChannel();
   return (
     <div>
       <label>Enter some text:</label>
-      <OptimisticTextarea onChange={(e) => pushMessage("text_input:changed", e.target.value)} value={state.text_input} clientVersion={clientVersion} serverVersion={serverVersion} />
+      <OptimisticTextarea onChange={(e) => pushMessage("text_input:changed", e.target.value)} value={state.text_input} />
     </div>
   );
 }
 
-const OptimisticTextarea = ({onChange, value, clientVersion, serverVersion}) => {
+const OptimisticTextarea = ({onChange, value}) => {
+  const { clientVersion, serverVersion } = useStateChannel();
   const [optimisticValue, setOptimisticValue] = useState(value);
   const didMountRef = useRef(false);
   const [cursorPosition, setCursorPosition] = useState(null);
@@ -47,58 +46,36 @@ const OptimisticTextarea = ({onChange, value, clientVersion, serverVersion}) => 
     <textarea ref={inputRef} onChange={(e) => handleChange(e)} value={optimisticValue}></textarea>
   )
 }
-export const Main = () => {
-  const [channel] = useChannel("app:state");
-  const [appState, setAppState] = useState();
-  const [serverVersion, setServerVersion] = useState(0);
-  const [clientVersion, setClientVersion] = useState(1);
 
-  useEffect(() => {
-    if (!channel) return;
-
-    const setStateRef = channel.on("set_state", ({state: state, version: version}) => {
-      setAppState(state);
-      setServerVersion(version);
-    });
-
-    const stateDiffRef = channel.on("state_diff", ({diff: diff, version: version}) => {
-      setAppState((state) => {
-        let newState = {...state};
-
-        jsondiffpatch.applyPatch(newState, diff)
-        console.debug(newState)
-        return newState;
-      });
-      setServerVersion(version);
-    });
-   
-    // stop listening to this message before the component unmounts
-    return () => {
-      channel.off("set_state", setStateRef);
-      channel.off("state_diff", stateDiffRef);
-    };
-  }, [channel]);
-
-  const pushMessage = (key, value) => {
-    setClientVersion((clientVersion) => clientVersion + 2);
-    channel.push(key, {value: value, version: clientVersion});
-  }
+const DebugState = () => {
+  const {state, clientVersion, serverVersion } = useStateChannel();
 
   return(
     <div>
-      { appState && <Form pushMessage={pushMessage} state={appState} clientVersion={clientVersion} serverVersion={serverVersion} />}
-      <h4>Current appState is:</h4>
+      <h4>Current state is:</h4>
       <pre>
-        { JSON.stringify(appState) }
+        { JSON.stringify(state) }
       </pre>
       <h4>Current serverVersion is: {serverVersion}</h4>
       <h4>Current clientVersion is: {clientVersion}</h4>
     </div>
   )
+};
+
+export const Main = () => {
+  return(
+    <PhoenixSocketProvider>
+      <StateChannelProvider channelName="app:state">
+        <Form />
+        <DebugState />
+      </StateChannelProvider>
+
+    </PhoenixSocketProvider>
+  )
 }
 
 window.addEventListener("load", () => {
   const root = ReactDOM.createRoot(document.getElementById("main"));
-  root.render(<PhoenixSocketProvider><Main /></PhoenixSocketProvider>)
+  root.render(<Main />)
 })
 
